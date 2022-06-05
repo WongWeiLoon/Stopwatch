2afeaa2534a7856557ab4911be85d069d5d52282
WeiLoon <weiloon.wong-prestatairep0@azqore.com>
2022-06-05 12:56:40 +0800

    Initialize project setup

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Welcome To React Native - StopWatch!</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  }, 
+});
+
+export default App;
